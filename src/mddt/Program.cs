45ed789f942ffbf4b7b4fa6fb2421a0038e9ae10
ulc <ulisses.cavalcante@altitude.com>
2019-03-06 13:05:57 -0300

    basic cli structure

--- src/mddt/Program.cs
+++ src/mddt/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace mddt
 {
     class Program
     {
+        private static Dictionary<string, string> dArgs = new Dictionary<string, string>();
+        private static string defaultSource = "https://casual-effects.com/markdeep/latest/markdeep.min.js";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i])
+                {
+                    case var t when (new List<string>() { "-o", "--output" }).Contains(t):
+                        if (args[i + 1].StartsWith("."))
+                        {
+                            var caminho = Environment.CurrentDirectory;
+                            dArgs.Add("output", args[i + 1]);
+                            i++;
+                        }
+                        else
+                        {
+                            var caminho = Path.GetPathRoot(args[i+1]);
+                            dArgs.Add("output", caminho);
+                            i++;
+                        }
+                        break;
+
+                    case var t when (new List<string>() { "-h", "--help" }).Contains(t):
+                        Console.WriteLine("Acertou Mizeravi");
+                        break;
+
+                        case var t when (new List<string>(){"-s", "--source"}).Contains(t):
+
+
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            foreach (var item in dArgs)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
